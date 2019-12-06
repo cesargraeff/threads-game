c80e26cc9159285f52c1ed777fc74b59e596c5e9
César Augusto Graeff <cesaraugustogff@gmail.com>
2019-12-06 14:55:20 -0300

    :sparkles: Cria o client

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,90 @@
-$(function(){
+(function () {
     'use strict';
 
-    
+    let socket;
+    let canvas;
 
-    function connect(){
+    const WS_URL = "ws://localhost:5000";
 
+    const images = [];
+
+    let timer = 0;
+
+    function init() {
+
+        let element = document.getElementById("canvas");
+
+        element.width = document.body.clientWidth;
+        element.height = document.body.clientHeight;
+
+        canvas = element.getContext("2d");
+        canvas.font = "10px Arial";
+
+        let img;
+        img = new Image();
+        img.src = 'img/tubarao.png'
+        images[0] = img;
+
+        img = new Image();
+        img.src = 'img/foca.png'
+        images[1] = img;
+
+        img = new Image();
+        img.src = 'img/peixe.png'
+        images[2] = img;
+
+        img = new Image();
+        img.src = 'img/alga.png'
+        images[3] = img;
+
+        connect();
     }
 
+    function connect() {
 
-    $(document).ready(function(){
-        connect();
+        socket = new WebSocket(WS_URL + '?width=' + document.body.clientWidth + '&height=' + document.body.clientHeight);
+        socket.onmessage = message;
+
+        setInterval(function() {
+            $('#timer').html((timer++)+'s');
+        }, 1000);
+    }
+
+    function message(e) {
+        const animals = JSON.parse(e.data);
+
+        let element = document.getElementById("canvas");
+        canvas.clearRect(0, 0, element.width, element.height);
+
+        $('#total').html(animals.length);
+
+        animals.forEach(e => {
+            desenha(e);
+        });
+    }
+
+    function desenha(e) {
+        canvas.drawImage(images[e.specie], e.x, e.y, 100, 50);
+        canvas.fillText('CALORIAS: '+e.calorias, e.x, e.y);
+    }
+
+    function add(animal) {
+        socket.send(JSON.stringify({
+            animal: animal,
+            calorias: parseInt($('#calorias').val())
+        }));
+    }
+
+    $(document).ready(function () {
+
+        $('.add').on("click", (e) => {
+            const animal = $(e.target).attr('type');
+            add(animal);
+        });
+
+        init();
     });
 
-
-})();;
+})();
 
 
